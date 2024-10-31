9a9ba1ef481c8373749e8270bb04a07891fc91b5
shlee0619 <shlee619@icloud.com>
2024-10-31 10:15:13 +0900

    test

--- 22okt.py
+++ 22okt.py
@@ -2,6 +2,7 @@
 
 from wordcloud import WordCloud, STOPWORDS
 from konlpy.tag import Okt
+import time
 
 okt = Okt()
 print(' Okt() 정보 ', okt) # <konlpy.tag._okt.Okt object at 0x00000228F4CC3BC0>
@@ -10,3 +11,17 @@ msg = '나는 과일중에 사과,체리 추출하여 워드 클라우드로 그
 print(msg)
 result1 = okt.nouns(msg)
 print(result1)
+print()
+time.sleep(0.5)
+
+result2 = okt.pos(msg)
+print(result2)
+print()
+
+result3 = okt.morphs(msg)
+print(result3)
+print()
+
+# msg2 = '아버지가방에들어가신다'
+# result2 = okt.nouns(msg2)
+# print(result2)
